131f0786a280ffc1a822e7a3d48db74cb1653ca0
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2023-12-07 14:45:35 +0100

    Extended transiently scoped data logger

--- src/data-logger/data-logger.service.ts
+++ src/data-logger/data-logger.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ConsoleLogger, Injectable, Scope } from '@nestjs/common';
 
-@Injectable()
-export class DataLoggerService {}
+@Injectable({ scope: Scope.TRANSIENT })
+export class DataLoggerService extends ConsoleLogger {
+  constructor() {
+    super();
+  }
+
+  create(type: string, key: string): void {
+    super.log(
+      `DATA | ${type} instance identified with ${key} was created at ${new Date().toLocaleString()}.`,
+    );
+  }
+
+  read(type: string, take: number): void {
+    super.log(
+      `DATA | ${type + take} ${
+        take > 1 ? 'instances were' : 'instance was'
+      } read at ${new Date().toLocaleString()}.`,
+    );
+  }
+
+  update(type: string, key: string, verbose?: string): void {
+    super.log(
+      `DATA | ${type} instance identified with ${key} was updated at ${new Date().toLocaleString()} (${verbose}).`,
+    );
+  }
+
+  delete(type: string, take: number): void {
+    super.log(
+      `DATA | ${type + take} ${
+        take > 1 ? 'instances were' : 'instance was'
+      } deleted at ${new Date().toLocaleString()}.`,
+    );
+  }
+}
