0a80469ba8008bb42d16c4819dd14540fde6c702
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2024-11-23 19:45:53 +0100

    Defined route for offeror image streaming and enabled offeror image upload

--- src/offerors/offerors.controller.ts
+++ src/offerors/offerors.controller.ts
@@ -6,6 +6,11 @@ import {
   Query,
   Patch,
   Param,
+  StreamableFile,
+  UseInterceptors,
+  UploadedFiles,
+  ParseFilePipe,
+  FileTypeValidator,
 } from '@nestjs/common';
 import { OfferorsService } from './offerors.service';
 import { PrivilegedRoute } from 'src/auth/privileged-route.decorator';
@@ -17,6 +22,8 @@ import { OfferorReputation } from './types';
 import AmendBusinessInfoDTO from './dto/amend-business-info.dto';
 import AlterReputationDTO from './dto/alter-reputation.dto';
 import User from 'src/auth/user.entity';
+import { createReadStream } from 'fs';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 
 @Controller('offerors')
 export class OfferorsController {
@@ -24,8 +31,20 @@ export class OfferorsController {
 
   @Post()
   @PrivilegedRoute('SUPERUSER')
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'highlight', maxCount: 1 },
+      { name: 'gallery', maxCount: 10 },
+    ]),
+  )
   recordOfferor(
     @Body() recordOfferorDTO: RecordOfferorDTO,
+    @UploadedFiles(
+      new ParseFilePipe({
+        validators: [new FileTypeValidator({ fileType: 'image/*' })],
+      }),
+    )
+    files: { highlight: Express.Multer.File; gallery: Express.Multer.File[] },
   ): Promise<{ id: string }> {
     return;
   }
@@ -36,6 +55,17 @@ export class OfferorsController {
     @Query() obtainOfferorsDTO: ObtainOfferorsDTO,
   ): Promise<Offeror[]> {
     return;
+  }
+
+  @Get('/:id/image')
+  obtainOfferorImage(
+    @Param()
+    @Query('destination')
+    destination: string,
+  ): StreamableFile {
+    const file = createReadStream(destination);
+
+    return new StreamableFile(file);
   }
 
   @Get('/business-info')
