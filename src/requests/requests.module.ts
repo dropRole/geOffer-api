7fe8db79d7858d4dd8f98e3534404577a5a72b03
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2024-12-25 16:36:18 +0100

    Altered the db schema

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -3,9 +3,10 @@ import { RequestsController } from './requests.controller';
 import { RequestsService } from './requests.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Request from './request.entity';
+import { ServiceToRequest } from './service-to-request';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Request])],
+  imports: [TypeOrmModule.forFeature([Request, ServiceToRequest])],
   controllers: [RequestsController],
   providers: [RequestsService],
 })
