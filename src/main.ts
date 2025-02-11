f5c2f4938e427d07fce2192f50573ad028482eff
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2025-02-11 15:15:50 +0100

    Configured CORS

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.GEOFFER_SPA_URL,
+      credentials: true,
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
